94a76c5d3ef5d7b5d5abe9187909478ab2df04b0
jnmclarty <jeffrey.mclarty@gmail.com>
2015-02-22 06:38:23 -0500

    Initial Commit

---
+++ sympa/core.py
@@ -0,0 +1,20 @@
+def _create_func(ex,lib):
+    symbs = list(ex.free_symbols)
+    names = [e.name for e in symbs]
+    s = sorted(zip(names,symbs))
+    names, symbs = zip(*s)
+    return lambdify(symbs,ex,lib), names
+    
+def domath(df,exp,lib='math'):    
+    afunc, names = _create_func(exp,lib)
+    
+    tdf = df.copy()
+    
+    for name in names:
+        if name not in tdf.columns:
+            if "_" in name:
+                v,i = name.split("_")
+                tdf[name] = tdf[v].shift(int(i)*-1)
+
+    vals = tdf[list(names)].values
+    return [afunc(*v) for v in vals]
